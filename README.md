e418bccebb8ed26a062f5b0c51d56966dbee4464
Mukund Kalra <mukundkalra@gmail.com>
2020-12-17 20:25:04 +0530

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# silicon-valley-btc
-The Bitcoin ALERT bot from Silicon Valley - now with more supported cryptocurrencies.
+# Gilfoyle's notifier when a cryptocurrency drops bellow a certain price.
+
+> Oh that's the song **You suffer by Napalm Death.**
+
+### Usage:
+
+After cloning the repository:
+
+```
+pip install requirements.txt
+
+python alert.py
+```
+
+By keeping the worker on background, it should notify you whenever a cryptocurrency drops bellow a specific price.
+
+**Supported cryptocurrencies:** BTC | BCH | ETH | LTC | ZEC
+
+<img src="gilf.gif">
+
+### Mukund Kalra [2020]
---
+++ alert.py
@@ -0,0 +1,68 @@
+import requests
+import schedule
+import time
+from pydub import AudioSegment
+from pydub.playback import play
+import os
+import sys
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("")
+
+
+cls()
+
+coin = input("Select a cryptocurrency: BTC | BCH | ETH | LTC | ZEC ")
+if coin.upper() not in ["BTC", "BCH", "ETH", "LTC", "ZEC"]:
+    print("Cryptocurrency not supported.")
+    exit()
+
+cls()
+
+value = input("Enter the price you bought your "+coin.upper()+" in USD: ")
+
+cls()
+
+print("Worker launched, you can leave it in background.")
+
+BELLOW = float(value)
+
+song = AudioSegment.from_mp3('clips\suffer.mp3')
+
+inc = 1
+
+
+def req_price():
+    r = requests.get(
+        'https://api.coinbase.com/v2/exchange-rates?currency='+coin.upper())
+    return float(r.json()['data']['rates']['USD'])
+
+
+def alert(price):
+    if price < float(BELLOW):
+        # mac notification
+        if sys.platform == 'darwin':
+            os.system("""
+                    osascript -e 'display notification "{}" with title "{}"'
+                    """.format("Current price: $"+str(price), coin.upper()+" price dropped bellow $"+str(BELLOW)))
+        play(song)
+
+
+def main():
+    global inc
+
+    if inc == 1:
+        alert(req_price())
+        inc += 1
+
+    if float(BELLOW) < req_price():
+        inc -= 1
+
+
+schedule.every(30).seconds.do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
